9f717acbc938fd68d8679a77a9033fa36a1e005e
이은수 <dldmstn0409@gmail.com>
2025-12-07 01:46:56 +0900

    준비중입니다로 변경

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@
 document.getElementById('downloadBtn').addEventListener('click', function(e) {
     e.preventDefault();
     // 실제 다운로드 링크로 교체하세요
-    alert('Demo download will begin soon!');
+    alert('Coming soon!');
     // window.location.href = 'your-download-link.zip';
 });
