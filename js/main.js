1660c1ee8ec691410f6beffafe32150adfb52e43
이은수 <dldmstn0409@gmail.com>
2025-12-07 01:30:53 +0900

    파일 구조 정리

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// 다운로드 버튼 클릭 이벤트
+document.getElementById('downloadBtn').addEventListener('click', function(e) {
+    e.preventDefault();
+    // 실제 다운로드 링크로 교체하세요
+    alert('데모 다운로드가 곧 시작됩니다!');
+    // window.location.href = 'your-download-link.zip';
+});
