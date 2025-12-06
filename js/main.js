8f742e03c4f2201fbc9f63760f1ad2059a030667
이은수 <dldmstn0409@gmail.com>
2025-12-07 01:46:10 +0900

    영어로 변경

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@
 document.getElementById('downloadBtn').addEventListener('click', function(e) {
     e.preventDefault();
     // 실제 다운로드 링크로 교체하세요
-    alert('데모 다운로드가 곧 시작됩니다!');
+    alert('Demo download will begin soon!');
     // window.location.href = 'your-download-link.zip';
 });
